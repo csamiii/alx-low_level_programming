75ac7ce3c688d601d8773678244b3750377646e9
csamiii <chidisamuel16@gmail.com>
2022-09-15 20:21:08 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print 0 - 9
+ *             only using _putchar twice
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+_putchar(c);
+_putchar('\n');
+}
