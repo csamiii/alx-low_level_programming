4e757a094477bdd30dee0938e9299f48a0b60450
csamiii <chidisamuel16@gmail.com>
2022-10-20 14:12:05 -0700

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * show_first - executes before the main function
+ *
+ * Return: nothing
+ */
+void __attribute__((constructor)) first()
+{
+printf("You're beat! and yet, you must allow");
+printf(",\nI bore my house upon my back!\n");
+}
