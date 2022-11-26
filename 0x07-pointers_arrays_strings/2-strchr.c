cca59fb88efab865b94e453960758d6d0dda52e3
csamiii <chidisamuel16@gmail.com>
2022-11-26 12:38:26 -0800

    locate a character in string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,13 @@
 
 char *_strchr(char *s, char c)
 {
-while (*s != '\0')
-{
-if (*s == c)
-return (s);
-s++;
-}
-if (*s == c)
-return (s);
-return ('\0');
+	int j, i, len=0;
+
+	for (j = 0; s[j] != '\0'; j++)
+		len++;
+
+	for (i = 0; i <= len; i++)
+		if (s[i] == c)
+			return (s + i);
+	return (s);
 }
